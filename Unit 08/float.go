c28a60b559950b94e9388d9fa5363545d560343b
Lee, Jae-Hong <pyrasis@gmail.com>
2015-06-30 18:35:04 +0900

    실수 표기 방법 설명 수정

--- Unit 08/float.go
+++ Unit 08/float.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	// 고정소수점 방식
+	// 소수점 사용
 	var num1 float32 = 0.1
 	var num2 float32 = .35
 	var num3 float32 = 132.73287
 
-	// 부동소수점 방식
+	// 지수 표기법
 	var num4 float32 = 1e7
 	var num5 float64 = .12345E+2
 	var num6 float64 = 5.32521e-10
