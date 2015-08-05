8d433c256ec9ee651d140499ea3dc8bc99befe8b
Lee, Jae-Hong <pyrasis@gmail.com>
2015-08-06 00:14:03 +0900

    인터페이스의 값 가져오기 예제 수정.

--- Unit 36/reflect_pointer_interface.go
+++ Unit 36/reflect_pointer_interface.go
@@ -18,9 +18,8 @@ func main() {
 	var b interface{}
 	b = 1
 
-	fmt.Println(reflect.TypeOf(b))               // int
-	fmt.Println(reflect.ValueOf(a))              // <*int Value>
-	fmt.Println(reflect.ValueOf(a).Int())        // 런타임 에러
-	fmt.Println(reflect.ValueOf(a).Elem())       // <int Value>
-	fmt.Println(reflect.ValueOf(a).Elem().Int()) // 1
+	fmt.Println(reflect.TypeOf(b))         // int
+	fmt.Println(reflect.ValueOf(b))        // <int Value>
+	fmt.Println(reflect.ValueOf(b).Int())  // 1
+	fmt.Println(reflect.ValueOf(b).Elem()) // 런타임 에러
 }
