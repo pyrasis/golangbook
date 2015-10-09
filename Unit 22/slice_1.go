152d5b4de5bf59a8ff3b42f3239e54245dd20c00
Lee, Jae-Hong <pyrasis@gmail.com>
2015-10-09 09:24:29 +0900

    슬라이스 선언 예제 수정

--- Unit 22/slice_1.go
+++ Unit 22/slice_1.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	var a []int = make([]int, 5) // make 함수로 int형에 길이가 5인 슬라이스에 공간 할당
 	var b = make([]int, 5)       // 슬라이스를 선언할 때 자료형과 [] 생략
-	c := make([], 5)             // 슬라이스를 선언할 때 var 키워드, 자료형과 [] 생략
+	c := make([]int, 5)          // 슬라이스를 선언할 때 var 키워드, 자료형과 [] 생략
 
 	fmt.Println(a)
 	fmt.Println(b)
